508890eceac56677b9a09444faa8d55c7cc3bc9a
Uwe Geuder <uwe.geuder@nomovok.com>
2012-05-29 15:52:37 +0300

    dev.rpm: configure postgresql to be started on boot

--- rpms/kata-ckan-dev/src/05setuppostgres.sh
+++ rpms/kata-ckan-dev/src/05setuppostgres.sh
@@ -6,6 +6,7 @@ pushd /var/lib/pgsql/data >/dev/null
 sudo -u postgres patch -p2 -i /usr/share/kata-ckan-dev/setup-patches/pg_hba.conf.patch
 popd >/dev/null
 service postgresql start
+chkconfig postgresql on
 # following command from "postgres createuser -e -S -D -R -P ckanuser"
 # couldn't find a way to avoid prompting for the password
 sudo -u postgres psql -c "CREATE ROLE ckanuser PASSWORD 'md5372712b8c6097730c3164ddd4f9275e0' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN"
