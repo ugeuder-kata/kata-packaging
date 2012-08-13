92585248041c1cd7e789b78df27c939b04309cb1
Aleksi Suomalainen <aleksi.suomalainen@nomovok.com>
2012-08-13 13:38:42 +0300

    Added chkconfig for rabbitmq and services to be on.

--- rpms/common/61setupsources.sh
+++ rpms/common/61setupsources.sh
@@ -3,6 +3,8 @@ set -x
 instloc=$1
 $instloc/pyenv/bin/paster --plugin=ckanext-harvest harvester source http://www.fsd.uta.fi/fi/aineistot/luettelo/fsd-ddi-records-uris-fi.txt DDI --config=/home/ckan/pyenv/src/ckan/development.ini
 $instloc/pyenv/bin/paster --plugin=ckanext-harvest harvester source http://helda.helsinki.fi/oai/request OAI-PMH --config=/home/ckan/pyenv/src/ckan/development.ini
+service rabbitmq-server start
+chkconfig rabbitmq-server on
 # Harvesting will only work after this
 service ckan-dev restart
 service supervisord restart
