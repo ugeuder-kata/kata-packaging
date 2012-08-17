fbb96d7e89881673423412e118082c52ad83609b
Uwe Geuder <uwe.geuder@nomovok.com>
2012-08-17 19:03:30 +0300

    rpm: ckan-dev init script & references to it removed
    dead code, replaced by httpd service already earlier

--- rpms/kata-ckan-dev/src/60installextensions.sh
+++ rpms/kata-ckan-dev/src/60installextensions.sh
@@ -3,5 +3,10 @@ set -x
 instloc=$1
 extensions="harvest oaipmh_harvester synchronous_search oaipmh ddi_harvester"
 sed -i "/^ckan.plugins/s|$| $extensions|" $instloc/pyenv/src/ckan/development.ini
-# make it so
-service ckan-dev restart
+# with the internal development server we used to call...
+#
+#   service ckan-dev restart
+#
+# ... here.
+# I don't think the equivalent is needed with Apache, because it should 
+# not have loaded any Python code yet
