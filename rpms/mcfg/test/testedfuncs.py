3c6bb7015c42a54cb22ffa0f215da81ce6a3a547
Uwe Geuder <uwe.geuder@nomovok.com>
2012-08-23 13:09:52 +0300

    mcfg: bug fixes to unittests & implementation
    4/5 tests pass

--- rpms/mcfg/test/testedfuncs.py
+++ rpms/mcfg/test/testedfuncs.py
@@ -2,6 +2,7 @@ import sys
 import os
 import inspect
 import filecmp
+import datetime
 
 # we live in subdirectory test, so we need to import the code to be tested
 # from the parent directory. I don't like this hack, please suggest something
@@ -21,15 +22,18 @@ class TestEdfuncs(unittest.TestCase):
 
   def test_replaceSuccessful(self):
     iFileName = os.path.join( self.testFilesDir , "replace1.in" )
-    print >> sys.stderr , "\n%s:%d: tempnam risk neglected, please suggest improvement" % ( __file__ , inspect.currentframe().f_lineno )
-    oFileName = os.tempnam( )
+
+    # originally we used os.tempnam() here but it clutters up the test output
+    # with a disturbing security warning.
+    oFileName = os.path.join( self.testFilesDir , "replaceSuccessful.out-" 
+                                        + datetime.datetime.now().isoformat())
+
     # we could call the editor function in a easier way here, but let's
     # simulate it once the way it will be done when the name comes from a 
     # text file
     func = getattr( edfuncs.edfuncs , "replace" )
     func( iFileName , oFileName , 'toBeReplaced' , 'replacement' )
-    # maybe by using difflib instead of filecmp we could replace tempnam
-    # by tmpfile???
+
     oFileNameExpected = os.path.join( self.testFilesDir , "replace1.out.expected" )
     result = filecmp.cmp( oFileName , oFileNameExpected )
     if result:
@@ -40,15 +44,25 @@ class TestEdfuncs(unittest.TestCase):
 
 
   def test_replaceInputFileMissing(self):
-    pass
-#        self.assertRaises(TypeError, random.shuffle, (1,2,3))
+    self.assertRaises(IOError, edfuncs.edfuncs.replace, "foo", "bar", "1", "2")
 
 
-  def test_copyFileSuccessFul(self):
-    pass
+  def test_copyFileSuccessful(self):
+    iFileName = os.path.join( self.testFilesDir , "replace1.in" )
+    oFileName = os.path.join( self.testFilesDir , "copyFileSuccessful.out-" 
+                                        + datetime.datetime.now().isoformat())
+    edfuncs.edfuncs.copyFile( "dummy" , oFileName , "location" , iFileName )
+    result = filecmp.cmp( iFileName , oFileName )
+    os.unlink( oFileName )
+    self.assertTrue( result )
+
 
   def test_CopyFileInputFileMissing(self):
-    pass
+    raise NotImplementedError
+
+  def test_CopyFileWrongParameter(self):
+    self.assertRaises( ValueError , edfuncs.edfuncs.copyFile , 
+                                   "dummy" , "dummy" , "wrong" , "dummy" )
 
 
 if __name__ == '__main__':
