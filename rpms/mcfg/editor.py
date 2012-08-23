acabcb1b76a23f23ce82af02e23819ed8411d295
Uwe Geuder <uwe.geuder@nomovok.com>
2012-08-23 18:39:36 +0300

    mcfg: editor class implemented, all unittests OK

--- rpms/mcfg/editor.py
+++ rpms/mcfg/editor.py
@@ -2,11 +2,23 @@ import edfuncs
 
 class editor:
 
-  def __init__(self, name, toFile, incr, fromStr, toStr):
-    self.edfunc = edfuncs.getmeth
-    self.incr = incr
-    self.toFile = toFile
-    self.fromStr = fromStr
-    self.toStr = toStr
+# an editor does not store information about its target. Target : editor
+# is 1 : n relation ship, so the details belong to the target. A reverse
+# pointer is not needed for our purpose.
 
-  def invoke( self, 
+# although currenently edfuncs take always 4 paramaters we just store a list
+# here. It doesn't make the code more complicated and allows to introduce
+# edfuncs with more parameters later
+
+# all naming with the semantics of the replace edfunc in mind  
+
+  def __init__(self, edFunc , incr, parList):
+    self.edFunc = edFunc
+    self.incr = incr
+    self.parList = parList
+
+  def runIt( self, fromFile, toFile) :
+    # incr ignored for the time being
+    func = getattr( edfuncs.edfuncs , self.edFunc )
+    func( fromFile , toFile , *self.parList )
+
