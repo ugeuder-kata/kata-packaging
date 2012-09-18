aee55a68631af98c718cb9a7130c7ab73aa5b6d5
Uwe Geuder <uwe.geuder@nomovok.com>
2012-09-18 15:49:30 +0300

    autobuild: bugfix, don't hardcode arch

--- autobuild/12buildinstallsolr.sh
+++ autobuild/12buildinstallsolr.sh
@@ -6,4 +6,4 @@ ln -s ../../rpms/solr/solr.spec
 rpmbuild -ba solr.spec
 arch=$(rpm -q --qf '%{arch}\n' --specfile solr.spec | head -n 1)
 cd ../RPMS/${arch}
-sudo yum install -y apache-solr-3.5.0-1.el6.i686.rpm
+sudo yum install -y apache-solr-3.5.0-1.el6.${arch}.rpm
