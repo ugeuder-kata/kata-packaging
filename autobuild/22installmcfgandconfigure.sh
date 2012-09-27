74c1f0d28306c1f6140415673665b0fb4d674b4d
Uwe Geuder <uwe.geuder@nomovok.com>
2012-09-27 19:15:30 +0300

    autobuild: phase 1 adapted to shibboleth installation (mcfg)
    tested OK

--- autobuild/22installmcfgandconfigure.sh
+++ autobuild/22installmcfgandconfigure.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 cd rpmbuild/RPMS/noarch
 sudo yum install -y mcfg-[0-9][0-9][0-9][0-9][0-9][0-9]-1.el6.noarch.rpm
+#TODO use host specific file from abuild-input
 sudo cp /usr/share/mcfg/examples/kata-master.ini /root
+cd
+sudo cp abuild-input/abuild-dev-sp-cert.pem /root/sp-cert.pem
+sudo cp abuild-input/abuild-dev-sp-cert.pem /root/sp-key.pem 
