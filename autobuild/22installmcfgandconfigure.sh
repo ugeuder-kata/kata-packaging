3519dc2a312496e3f46466df20cb2be6b1e05a67
Uwe Geuder <uwe.geuder@nomovok.com>
2012-10-04 13:47:00 +0300

    autobuild: optionally use input file as master config
    not yet tested

--- autobuild/22installmcfgandconfigure.sh
+++ autobuild/22installmcfgandconfigure.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 cd rpmbuild/RPMS/noarch
 sudo yum install -y mcfg-[0-9][0-9][0-9][0-9][0-9][0-9]-1.el6.noarch.rpm
-#TODO use host specific file from abuild-input
-sudo cp /usr/share/mcfg/examples/kata-master.ini /root
 cd
+if [ -r abuild-input/abuild-dev-kata-master.ini ]
+then
+  sudo cp abuild-input/abuild-dev-kata-master.ini /root/kata-master.ini
+else
+  sudo cp /usr/share/mcfg/examples/kata-master.ini /root
+fi
+
 sudo cp abuild-input/abuild-dev-sp-cert.pem /root/sp-cert.pem
 sudo cp abuild-input/abuild-dev-sp-key.pem /root/sp-key.pem 
--- autobuild/40installprod.sh
+++ autobuild/40installprod.sh
@@ -2,8 +2,12 @@
 tar xf rpms-*.tar
 cd allrpms
 sudo yum install -y mcfg-*.rpm
-#TODO use host specific file from abuild-input
-sudo cp /usr/share/mcfg/examples/kata-master.ini /root
+if [ -r abuild-input/abuild-prod-kata-master.ini ]
+then
+  sudo cp abuild-input/abuild-prod-kata-master.ini /root/kata-master.ini
+else
+  sudo cp /usr/share/mcfg/examples/kata-master.ini /root
+fi
 sudo cp ~/abuild-input/abuild-prod-sp-cert.pem /root/sp-cert.pem
 sudo cp ~/abuild-input/abuild-prod-sp-key.pem /root/sp-key.pem 
 sudo yum install -y apache-solr-*.rpm
