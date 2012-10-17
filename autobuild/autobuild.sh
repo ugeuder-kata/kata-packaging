271987518fbc31f0239e87bd5f22db508941e5b6
Uwe Geuder <uwe.geuder@nomovok.com>
2012-10-17 21:29:53 +0300

    autobuild: make buildhost and port configurable
    not all combinations tested yet

--- autobuild/autobuild.sh
+++ autobuild/autobuild.sh
@@ -3,6 +3,34 @@ abuilduser=abuild
 abuildkey=~/id_rsa_abuild
 abuildhostdev=abuild
 abuildhostprod=abuild2
+abuildhostdevport=22
+abuildhostprodport=22
+if [ \! -z "$ABUILDHOST" ]
+then
+  abuildhostdev=$ABUILDHOST
+  abuildhostprod=$ABUILDHOST
+fi
+if [ \! -z "$ABUILDHOSTDEV" ]
+then
+  abuildhostdev=$ABUILDHOSTDEV
+fi
+if [ \! -z "$ABUILDHOSTPROD" ]
+then
+  abuildhostprod=$ABUILDHOSTPROD
+fi
+if [ \! -z "$ABUILDPORT" ]
+then
+  abuildportdev=$ABUILDPORT
+  abuildportprod=$ABUILDPORT
+fi
+if [ \! -z "$ABUILDPORTDEV" ]
+then
+  abuildportdev=$ABUILDPORTDEV
+fi
+if [ \! -z "$ABUILDPORTPROD" ]
+then
+  abuildportprod=$ABUILDPORTPROD
+fi
 # requirements: access with ssh key $abuildkey must be authorized by 
 #               user $abuild
 # for further requirements see README
@@ -17,7 +45,7 @@ function rexec() {
   $SETCOLOR
   echo $*
   $RESETCOLOR
-  ssh -i $abuildkey ${abuilduser}@${abuildhost} $*
+  ssh -i $abuildkey -p $abuildport ${abuilduser}@${abuildhost} $*
 }
 
 set -x
@@ -28,8 +56,9 @@ $RESETCOLOR
 case $phase in
   1)
     abuildhost=$abuildhostdev
+    abuildport=$abuildportdev
     tar cf abuild.tar .
-    scp -i $abuildkey abuild.tar ${abuilduser}@${abuildhost}:
+    scp -i $abuildkey -P $abuildport abuild.tar ${abuilduser}@${abuildhost}:
     rexec tar xf abuild.tar
     rexec autobuild/04addrepos.sh
     rexec autobuild/08setuprpmbuild.sh
@@ -43,29 +72,31 @@ case $phase in
     $RESETCOLOR ;;
   2)
     abuildhost=$abuildhostdev
+    abuildport=$abuildportdev
     rexec autobuild/28stopservices.sh
     rexec autobuild/32buildkataprod.sh
     rexec autobuild/36tarrpms.sh
     e=$(date +%s)
     min=$(($e/60))
     version=$(($min%1000000))
-    scp -i $abuildkey ${abuilduser}@${abuildhost}:rpmbuild/RPMS/rpms.tar rpms-${version}.tar
+    scp -i $abuildkey -P $abuildport ${abuilduser}@${abuildhost}:rpmbuild/RPMS/rpms.tar rpms-${version}.tar
     $SETCOLOR
-    if [ "$abuildhostdev" = "$abuildhostprod" ]
+    if [ "$abuildhostdev" = "$abuildhostprod" -a "$abuildportdev" = "$abuildportprod"]
     then
-      echo "Reset host $abuild for phase 3"
+      echo "Reset host ${abuildhost}:${abuildport} for phase 3"
     else
-      echo "Make sure $abuildhostprod has been reset before continuing with phase 3" 
+      echo "Make sure ${abuildhostprod}:${abuildportprod} has been reset before continuing with phase 3" 
     fi
     $RESETCOLOR ;;
 
   3)
     abuildhost=$abuildhostprod
-    scp -i $abuildkey $(ls rpms-*.tar | tail -1) ${abuilduser}@${abuildhost}:
+    abuildport=$abuildportprod
+    scp -i $abuildkey -P $abuildport $(ls rpms-*.tar | tail -1) ${abuilduser}@${abuildhost}:
     rexec mkdir autobuild
-    scp -i $abuildkey autobuild/04addrepos.sh ${abuilduser}@${abuildhost}:autobuild/04addrepos.sh
+    scp -i $abuildkey -P $abuildport autobuild/04addrepos.sh ${abuilduser}@${abuildhost}:autobuild/04addrepos.sh
     rexec autobuild/04addrepos.sh
-    scp -i $abuildkey autobuild/40installprod.sh ${abuilduser}@${abuildhost}:autobuild/40installprod.sh
+    scp -i $abuildkey -P $abuildport autobuild/40installprod.sh ${abuilduser}@${abuildhost}:autobuild/40installprod.sh
     rexec autobuild/40installprod.sh ;;
   *)
     echo "usage: $0 [1|2|3]"
