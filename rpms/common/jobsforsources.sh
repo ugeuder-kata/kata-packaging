f600c522a8216d08b2aa68c0e4f86d605449eae7
Aleksi Suomalainen <aleksi.suomalainen@nomovok.com>
2012-11-08 14:57:55 +0200

    Fix cronjob script.

--- rpms/common/jobsforsources.sh
+++ rpms/common/jobsforsources.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 . /home/ckan/pyenv/bin/activate
-sources=`/home/ckan/pyenv/bin/paster --plugin=ckanext-harvest harvester sources|grep "Source id:"|cut -c 12-`
+sources=`/home/ckan/pyenv/bin/paster --plugin=ckanext-harvest harvester sources --config=/home/ckan/pyenv/src/ckan/development.ini |grep "Source id:"|cut -c 12-`
 for source in sources;
 do
-    /home/ckan/pyenv/bin/paster --plugin=ckanext-harvest harvester job $source > /dev/null 2>&1;
+    /home/ckan/pyenv/bin/paster --plugin=ckanext-harvest harvester job $source --config=/home/ckan/pyenv/src/ckan/development.ini  > /dev/null 2>&1;
 done
 /home/ckan/pyenv/bin/paster --plugin=ckanext-harvest harvester run --config=/home/ckan/pyenv/src/ckan/development.ini > /dev/null 2>&1
