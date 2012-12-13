b5f9e649d5821d4c1510a274c499bd729f8f2a94
Uwe Geuder <uwe.geuder@nomovok.com>
2012-12-13 10:21:40 +0200

    dev.rpm: exclude generated random values from patching

--- rpms/kata-ckan-dev/src/28setupckan.sh
+++ rpms/kata-ckan-dev/src/28setupckan.sh
@@ -11,7 +11,21 @@ cd $instloc
 source pyenv/bin/activate
 cd pyenv/src/ckan
 paster make-config ckan development.ini
+
+# if generated random stuff is too close to lines we need to patch
+# patch command will complain and eventually reject hunks
+#
+# extract generated random stuff before patching
+# keep intermediate in /tmp just in case we need to debug. This is the
+# development machine, no need to clean up
+cp development.ini development.generated-orig
+grep -e ^beaker\.session.\secret -e ^app_instance_uuid development.ini >/tmp/development.ini.generated-only
+sed -i -e "/^beaker\.session\.secret/ d" -e s/^app_instance_uuid.*/%%GENERATED%%/ development.ini
 patch -b -p2 -i /usr/share/kata-ckan-dev/setup-patches/development.ini.patch
+mv development.ini development.ini.patched
+sed -e "/%%GENERATED%%/" r /tmp/development.ini.generated-only" -e /%%GENERATED%%/ d" development.ini.patched > development.ini
+# patching done...
+
 myipcmd=$(dirname $0)/myip.sh
 myip=$($myipcmd)
 # TODO: needs to be really configurable
